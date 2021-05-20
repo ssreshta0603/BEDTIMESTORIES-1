0ad43dc2ae4fc0097bdce4dc84060fe695a0b6ff
Sreshta0603 <67061788+ssreshta0603@users.noreply.github.com>
2021-05-20 16:29:43 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,14 +1,13 @@
 import firebase from 'firebase'
 require('@firebase/firestore')
 
-var firebaseConfig = {
-    apiKey: "AIzaSyBVahlxK6TGfwdRyM5xeX0RH4ZOIbGRLS0",
-    authDomain: "storyhub-bda18.firebaseapp.com",
-    databaseURL: "https://storyhub-bda18-default-rtdb.firebaseio.com",
-    projectId: "storyhub-bda18",
-    storageBucket: "storyhub-bda18.appspot.com",
-    messagingSenderId: "393024213830",
-    appId: "1:393024213830:web:6738e2ba923404f5b88b47"
+ var firebaseConfig = {
+    apiKey: "AIzaSyACwdnIwc_3DEOIN8wG89GJkNmkbTKtO-A",
+    authDomain: "storyhub-d8984.firebaseapp.com",
+    projectId: "storyhub-d8984",
+    storageBucket: "storyhub-d8984.appspot.com",
+    messagingSenderId: "1071716011747",
+    appId: "1:1071716011747:web:900c3b4ff743152d3c7fd6"
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
